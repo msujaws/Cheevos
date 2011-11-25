a0cb8dd20ae121ee03e0598a7e59f1c01e5e3e9e
Jared Wein <weinjared@gmail.com>
2011-11-25 18:36:01 -0500

    start of twitter share button

--- data/cheevo.js
+++ data/cheevo.js
@@ -5,4 +5,13 @@ function populateTemplate(name, message, title, cheevoBlocks) {
   document.getElementById("cheevo-h2").textContent = unescape(title);
   document.getElementById("cheevo-message").textContent = unescape(message);
   document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
+
+  let shareClicked = false;
+  let twitterLink = document.getElementById("twitterLink");
+  document.getElementById("share").addEventListener("click", function () {
+    if (!shareClicked) {
+      shareClicked = true;
+      this.insertAdjacentHTML('afterend', '<a href="https://twitter.com/share" class="twitter-share-button" data-url="https://www.mozilla.org" data-text="Cheevo for Firefox achieved!" data-count="none" data-via="firefox" data-related="weinjared:Author of Cheevos for Firefox">Tweet</a><script type="text/javascript" src="https://platform.twitter.com/widgets.js"></script>');
+    }
+  }, false);
 }
