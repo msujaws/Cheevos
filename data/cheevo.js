3926296613a2894cd1976906a1988bc0881d0e42
Matt Wein <mwein2009@gmail.com>
2011-11-25 03:24:59 -0500

    created HTML template with blocks for each cheevo

--- data/cheevo.js
+++ data/cheevo.js
@@ -1,8 +1,9 @@
-function populateTemplate(name, message, title, cheevoList) {
+function populateTemplate(name, message, title, cheevoList, cheevoBlocks) {
   if (!document.getElementById("cheevo-h1"))
     return;
   document.getElementById("cheevo-h1").textContent = unescape(name);
   document.getElementById("cheevo-h2").textContent = unescape(title);
   document.getElementById("cheevo-message").textContent = unescape(message);
   document.getElementById("cheevo-wrapper").innerHTML = unescape(cheevoList);
+  document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
 }
