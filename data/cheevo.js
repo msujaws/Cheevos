56a76a635268a4b3fd88b66a58a35e4775a9175e
Matt Wein <mwein2009@gmail.com>
2011-11-25 01:49:21 -0500

    added css

--- data/cheevo.js
+++ data/cheevo.js
@@ -4,5 +4,5 @@ function populateTemplate(name, message, title, cheevoList) {
   document.getElementById("cheevo-h1").textContent = unescape(name);
   document.getElementById("cheevo-h2").textContent = unescape(title);
   document.getElementById("cheevo-message").textContent = unescape(message);
-  document.getElementById("cheevo-ul").innerHTML = unescape(cheevoList);
+  document.getElementById("cheevo-wrapper").innerHTML = unescape(cheevoList);
 }
