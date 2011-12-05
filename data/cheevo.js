c4a22a008cfb9b4ec97cb4231fc2252b48d83835
Jared Wein <weinjared@gmail.com>
2011-12-04 17:04:37 -0800

    renamed messages to be descriptions

--- data/cheevo.js
+++ data/cheevo.js
@@ -1,4 +1,4 @@
-function populateTemplate(name, message, title, cheevoBlocks) {
+function populateTemplate(name, description, title, cheevoBlocks) {
   if (!document.getElementById("cheevo-h1"))
     return;
   document.getElementById("cheevo-h1").textContent = unescape(name);
@@ -7,7 +7,7 @@ function populateTemplate(name, message, title, cheevoBlocks) {
     titleEl.textContent = unescape(title);
   else
     titleEl.parentNode.removeChild(titleEl);
-  document.getElementById("cheevo-message").textContent = unescape(message);
+  document.getElementById("cheevo-message").textContent = unescape(description);
   document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
   let twitterLink = document.getElementById("twitterLink");
   twitterLink.setAttribute("data-text", twitterLink.getAttribute("data-text").replace("#1", unescape(name)));
