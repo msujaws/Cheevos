e4bf779ad064c75d2db03dae593a0a54f729a95a
Jared Wein <weinjared@gmail.com>
2011-12-05 21:26:18 -0800

    the url of the cheevos page is now about:cheevos

--- data/cheevo.js
+++ data/cheevo.js
@@ -1,14 +1,15 @@
-function populateTemplate(name, description, title, cheevoBlocks) {
+function populateTemplate(name, description, title, cheevoBlocks, styleCss) {
   if (!document.getElementById("cheevo-h1"))
     return;
   document.getElementById("cheevo-h1").textContent = unescape(name);
   let titleEl = document.getElementById("cheevo-h2");
   if (title)
     titleEl.textContent = unescape(title);
-  else
+  else if (titleEl)
     titleEl.parentNode.removeChild(titleEl);
   document.getElementById("cheevo-message").textContent = unescape(description);
   document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
+  document.getElementById("cheevo-stylesheet").href = unescape(styleCss);
   let twitterLink = document.getElementById("twitterLink");
   twitterLink.setAttribute("data-text", title ? twitterLink.getAttribute("data-text").replace("#1", unescape(name)) : unescape(name));
   document.getElementById("twitterScript").src = 'https://platform.twitter.com/widgets.js';
