ef4f66272b7f6fe577b01ca1587ee3955787e842
Jared Wein <weinjared@gmail.com>
2011-12-25 16:47:27 -0800

    updated cheevo page text to show the point total

--- data/cheevo.js
+++ data/cheevo.js
@@ -1,16 +1,11 @@
-function populateTemplate(name, description, title, cheevoBlocks, styleCss) {
+function populateTemplate(tweetText, stats, cheevoBlocks, styleCss) {
   if (!document.getElementById("cheevo-h1"))
     return;
   document.getElementById("cheevo-h1").textContent = "Cheevos for Firefox";
-  let titleEl = document.getElementById("cheevo-h2");
-  if (title)
-    titleEl.textContent = unescape(title);
-  else if (titleEl)
-    titleEl.parentNode.removeChild(titleEl);
-  document.getElementById("cheevo-message").textContent = unescape(description);
+  document.getElementById("cheevo-h2").textContent = "Use Firefox, gain achievements.";
+  document.getElementById("cheevo-message").textContent = unescape(stats);
   document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
   document.getElementById("cheevo-stylesheet").href = unescape(styleCss);
-  let twitterLink = document.getElementById("twitterLink");
-  twitterLink.setAttribute("data-text", unescape(name));
+  document.getElementById("twitterLink").setAttribute("data-text", unescape(tweetText));
   document.getElementById("twitterScript").src = 'https://platform.twitter.com/widgets.js';
 }
