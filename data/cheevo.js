16fee3cc4854a063f9af74ee20b08d7364dfdedd
Matt Wein <mwein2009@gmail.com>
2012-07-28 17:47:44 -0700

    Moved more strings to the properties file

--- data/cheevo.js
+++ data/cheevo.js
@@ -1,8 +1,8 @@
-function populateTemplate(tweetText, stats, cheevoBlocks, styleCss) {
+function populateTemplate(tweetText, stats, cheevoBlocks, styleCss, cheevosFullName, cheevosTagLine) {
   if (!document.getElementById("cheevo-h1"))
     return;
-  document.getElementById("cheevo-h1").textContent = "Cheevos for Firefox";
-  document.getElementById("cheevo-h2").textContent = "Use Firefox, gain achievements.";
+  document.getElementById("cheevo-h1").textContent = unescape(cheevosFullName);
+  document.getElementById("cheevo-h2").textContent = unescape(cheevosTagLine);
   document.getElementById("cheevo-message").textContent = unescape(stats);
   document.getElementById("cheevo-blocks").innerHTML = unescape(cheevoBlocks);
   document.getElementById("cheevo-stylesheet").href = unescape(styleCss);
